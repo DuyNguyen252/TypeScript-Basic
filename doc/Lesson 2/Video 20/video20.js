56bfe9bfbccec6073a41d285da0db646ddd81716
Duy Nguyen <emailcuaban@gmail.com>
2026-05-11 15:52:53 +0700

    update exercises

--- doc/Lesson 2/Video 20/video20.js
+++ doc/Lesson 2/Video 20/video20.js
@@ -1,7 +1,25 @@
 console.log("Video 20");
 
-const a = 5;
-const b = 10, c = 15;
+// let a = 5;
+// // const b = 10, c = 15;
 
-const sum = a + c - b;
-console.log(sum);
+// // const sum = a + c - b;
+// // console.log(sum);
+
+// // const d = a * b - c;
+// // console.log(d);
+
+// // console.log(c % a);
+// const b = a++; //b = a + 1
+// // const c = ++a; //c = a + 1
+
+// console.log(a);
+// console.log(b);
+// // console.log(c);
+
+const a = 5; //number
+const b = "5"; // string
+console.log(a == b); //true so sanh gia tri 
+console.log(a === b); //false so sanh kieu du lieu va gia tri
+
+
---
+++ doc/Lesson 2/Video 21/video21.js
@@ -0,0 +1,20 @@
+console.log("Video 21");
+
+// const name = "andy";
+// const age = 21;
+
+// console.log("info");
+// console.error("This is an error message");
+// console.warn("This is a warning message");
+// // console.info("This is an info message");
+
+
+let name = "AndyNguyen";
+let age = 20;
+
+// Không nên: cộng chuỗi dễ sai
+// console.log("Name: " + name + ", Age: " + age);
+
+console.log("Name: ", name);
+console.log("Age:",age,", years old" , age);
+console.log("Name: " + name + ", Age: " + age);
